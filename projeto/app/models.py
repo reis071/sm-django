15007084b66cd59eec99c7d0d92f08c4d916e460
reis071 <reiscorreia071@gmail.com>
2025-03-07 21:20:25 -0300

    add aula 04 - sql e model

--- projeto/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Produto(models.Model):
-    nome = models.CharField(max_length=60)
-    marca = models.CharField(max_length=50)
-    preco = models.DecimalField(max_digits = 10 , decimal_places = 2)
-    estoque = models.IntegerField()
-    
--- projeto/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('listar_produtos',views.buscar_todos_os_produtos, name= 'rota_lista_de_produtos'),
-    path('cadastrar_produto',views.cadastrar_produto,name='rota_de_cadastrar_produto'),
-    path('cadastar_usuario',views.cadastrar_usuario),
-    path('logar_usuario',views.login_usuario, name='rota_login'),
-]
